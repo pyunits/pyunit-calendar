95ad6d2d66dc7912fcc75458ad9d838c85202121
jtyoui <jtyoui@qq.com>
2020-02-05 17:07:14 +0800

    优化算法

--- pyunit_calendar/__init__.py
+++ pyunit_calendar/__init__.py
@@ -7,7 +7,7 @@ from .CTC_ import CTC  # 农历转阳历
 from .BatchCalendar import load_date, td_to_ctc, td_to_sc, ctc_to_sc, ctc_to_td, sc_to_ctc, sc_to_td  # 批量转换日历
 from .LSC import LunarDate, SolarDate, LunarSolarDateConverter  # 阴历和阳历转换
 
-__version__ = '2019-05-09'
+__version__ = '2019.05.09'
 __author__ = 'Jtyoui'
 __description__ = '常见的日历转换器'
 __email__ = 'jtyoui@qq.com'
